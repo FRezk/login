8211eb45a6ab3a07efee3faa81d17cb8cb0bb00d
Fernando Rezk <fernandorezk@outlook.com>
2018-07-15 23:00:31 -0300

    Last changes

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -1,9 +1,18 @@
 $(() => {
     $("#loginForm").on('submit', event => {
         event.preventDefault()
-        alert("WORKS")
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/posts/1", //"localhost:8080/id8-api/tryLogin", // https://jsonplaceholder.typicode.com/posts/1
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            type: "GET",
+            success: result => {
+                console.log("Point to our own webservice to validate!")
+            }
+        })
         const username = event.target.email.value
         const password = event.target.password.value
-        console.log(username, password)
+        //console.log(username, password)
     })
 })
